ec9794dfe7514e59260e4ec150043e6d4ee5f445
railsbeginner <bartosz.dyja@gmail.com>
2015-09-03 16:06:45 +0200

    Add website model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902203210) do
+ActiveRecord::Schema.define(version: 20150903140621) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -31,5 +31,11 @@ ActiveRecord::Schema.define(version: 20150902203210) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "websites", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
