95074d08bebf05d22616baa8dedc914b4d61f8b4
railsbeginner <bartosz.dyja@gmail.com>
2015-09-03 16:19:10 +0200

    Configure basic views

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -2,5 +2,6 @@ class WebsitesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @websites = current_user.websites
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903140621) do
+ActiveRecord::Schema.define(version: 20150903140734) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20150903140621) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "websites", ["user_id"], name: "index_websites_on_user_id"
 
 end
